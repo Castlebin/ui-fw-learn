94cca6d0d69317bfcd5fe71c201c5417a4a7ccf9
Castlebin <Castlebin@gmail.com>
2025-02-22 14:42:12 +0800

    1. 创建和嵌套组件、使用组件

---
+++ react_test/src/components/MyApp.tsx
@@ -0,0 +1,12 @@
+// 导入并使用 MyButton 组件
+
+import MyButton from "./MyButton";
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>欢迎来到我的应用</h1>
+      <MyButton />  {/* 使用 MyButton 组件 */}
+    </div>
+  );
+}
---
+++ react_test/src/components/MyButton.tsx
@@ -0,0 +1,6 @@
+
+// 定义并导入了一个 React 组件，名字为 MyButton
+// React 组件是返回标签的 JavaScript 函数。这个函数的名字必须以大写字母开头
+export default function MyButton() {
+  return <button>我是一个按钮</button>;
+}
