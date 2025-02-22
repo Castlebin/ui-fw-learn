dd36863a3275deb8e923486d7f52c19657cbb7c1
Castlebin <Castlebin@gmail.com>
2025-02-22 15:06:45 +0800

    6. 响应事件

--- react_test/src/components/MyButton.tsx
+++ react_test/src/components/MyButton.tsx
@@ -2,5 +2,11 @@
 // 定义并导入了一个 React 组件，名字为 MyButton
 // React 组件是返回标签的 JavaScript 函数。这个函数的名字必须以大写字母开头
 export default function MyButton() {
-  return <button>我是一个按钮</button>;
+
+  function handleClick() {
+    alert('按钮被点击了');
+    console.log('用户点击了按钮');
+  }
+
+  return <button onClick={handleClick}>我是一个按钮</button>;
 }
